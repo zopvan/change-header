49ab6aede24ddb13042b435213a3c9075e24bacf
zopvan <zooganz1@gmail.com>
2025-12-29 23:10:39 +0700

    add documentation

--- main.py
+++ main.py
@@ -9,29 +9,31 @@ def rusak_satu_folder(nama_folder):
 
     print(f"--- MULAI OPERASI PADA FOLDER: {nama_folder} ---")
     
-    # Ambil daftar semua item di dalam folder
+    # Lihat dan Ambil daftar semua item di dalam folder
     list_file = os.listdir(nama_folder)
     
-    # === VALIDASI FOLDER KOSONG ===
+    # validasi apakah folde kosong??
     if len(list_file) == 0:
         print(f"⚠️  VALIDASI: Folder '{nama_folder}' KOSONG coy.")
         print("   Tidak ada file yang diubah headernya.")
-        return  # <-- Stop di sini, kembali ke menu utama
+        return  # <-- jika tidak ada akan berhenti di sini dan kembali ke menu utama
     # =====================================
 
     jumlah_sukses = 0
 
-    # 2. Lakukan Looping (Perulangan) untuk setiap file
+    # 2. Melakukan Looping (Perulangan) 
+    # untuk setiap file untuk memproses 1 per 1
     for nama_file in list_file:
-        full_path = os.path.join(nama_folder, nama_file)
+        full_path = os.path.join(nama_folder, nama_file) #memberi tahu full path (contoh file foto.jpg terdapat pada src maka src/foto.jpg)
 
         # 3. validasi proses adalah FILE (bukan folder lain)
+        # jadi kalo folder dia akan skip prosesnya
         if os.path.isfile(full_path):
             try:
-                with open(full_path, 'r+b') as f:
-                    header_acak = os.urandom(16)
-                    f.seek(0)
-                    f.write(header_acak)
+                with open(full_path, 'r+b') as f: #membuka file sebagai membaca dan menulis file dalam bentuk binary
+                    header_acak = os.urandom(16) #menghasilkan str byte acak sebanyak 16byte
+                    f.seek(0) #memindah kursor ke awal pada header file
+                    f.write(header_acak) #menimpa 16 byte awal pada header file
                 
                 print(f"[RUSAK] {nama_file} -> Header diganti random.")
                 jumlah_sukses += 1
@@ -42,7 +44,8 @@ def rusak_satu_folder(nama_folder):
             print(f"[SKIP] {nama_file} adalah folder, dilewati.")
 
     print("-" * 40)
-    # Validasi tambahan: Jika folder ada isinya tapi isinya folder semua (bukan file)
+    
+    # menampilkan status, header sukses ditimpa atau tidak 
     if jumlah_sukses == 0:
         print(f"Info: Folder '{nama_folder}' tidak kosong, tapi tidak ada FILE yang bisa dirusak.")
     else:
